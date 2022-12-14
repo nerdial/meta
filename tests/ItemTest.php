3e996aa8f4c1456a250c28a9ef0dd961024565e4
VM <Vahid>
2022-12-15 01:03:33 +0330

    Add item and auction test

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -8,12 +8,12 @@ use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 class ItemTest extends ApiTestCase
 {
 
-    use RefreshDatabaseTrait;
+//    use RefreshDatabaseTrait;
 
     public function testCreateNewItem(): void
     {
 
-        $url = 'api/items';
+        $url = '/api/items';
         $newItem = [
             'title' => 'first item',
             'user' => '/api/users/1'
@@ -27,5 +27,40 @@ class ItemTest extends ApiTestCase
         ]);
     }
 
+
+    public function testCreateNewItemAndThenCreateAuction(): void
+    {
+
+        $itemUrl = '/api/items';
+        $auctionUrl = '/api/auctions';
+        $newItem = [
+            'title' => 'second item',
+            'user' => '/api/users/1'
+        ];
+        $res = static::createClient()->request(method: 'POST', url: $itemUrl, options: [
+            'json' => $newItem
+        ]);
+
+        $itemId = $res->toArray()['@id'];
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            'title' => $newItem['title']
+        ]);
+
+        $res = static::createClient()->request(method: 'GET', url: $itemUrl);
+
+        $newAuction = [
+            'price' => 1000,
+            'item' => $itemId
+        ];
+
+        static::createClient()->request(method: 'POST', url: $auctionUrl, options: [
+            'json' => $newAuction
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains($newAuction);
+
+    }
 
 }
